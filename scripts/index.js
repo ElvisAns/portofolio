9aa79b8f1f9b368d275caea94128d4bca370ace9
ElvisAns <ansimapersic@gmail.com>
2022-02-17 17:05:53 +0200

    Fist commit

---
+++ scripts/index.js
@@ -0,0 +1,154 @@
+const index = {};
+
+index.$menu = $('#sideMenu');
+index.$navItem = $('.navItem');
+index.$navItemName = $('.navItemName')
+index.$home = $('#home');
+index.$about=$('#about');
+index.$skills = $('#skills');
+index.$works = $('#works');
+index.$contact = $('#contact');
+index.$menuButton = $('#menuButton');
+index.$scrollDown = $('#scrollDown');
+index.isOpen = false;
+
+// Scroll function
+index.scroll = function(target) {
+  $('html,body').animate({ scrollTop: $(target).offset().top }, 500);
+}
+
+// Menu button function
+index.showHideMenu = function() {
+  index.$menu.toggleClass('sideMenuHide sideMenuShow');
+  index.isOpen = !index.isOpen;
+}
+
+// Mobile class changes on initial load
+if ($(window).width() <= 990) {
+  index.$menu.addClass('sideMenuHide').removeClass('sideMenuShow');
+  index.$scrollDown.hide();
+}
+
+index.eventListeners = function() {
+  // when windox resizes between large and small displayes
+  $(window).on('resize', function() {
+    if ($(window).width() > 990) {
+      index.$menu.removeClass('sideMenuHide').addClass('sideMenuShow')
+      index.$scrollDown.show();
+      index.isOpen= false;
+    } else {
+      index.$menu.removeClass('sideMenuShow').addClass('sideMenuHide');
+      index.isOpen = false;
+      index.$scrollDown.hide();
+    }
+  });
+
+  // Nav menu click events for scroll function
+  $('a[href*=\\#]').on('click', function () {
+    index.scroll(this.hash);
+  });
+
+  // Menu button click event
+  index.$menuButton.on('click', index.showHideMenu);
+
+  // Hide menu when clicking a link
+  index.$navItem.on('click', function() {
+    if (index.isOpen) {// Hide menu when clicking a link
+      index.showHideMenu();
+    }
+  });
+
+  // Menu button ENTER key event
+  index.$menuButton.on('keypress', function(e){
+    if (e.which === 13) {
+      $(this).trigger('click');
+    }
+  })
+}
+
+// init method
+index.init = function() {
+  index.eventListeners();
+}
+
+// Document Ready
+$(function() {
+  index.init();
+})
+
+tsParticles.load("tsparticles", {
+  fps_limit: 30,
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onclick: { enable: true, mode: "push" },
+      onhover: {
+        enable: true,
+        mode: "attract",
+        parallax: { enable: false, force: 60, smooth: 10 }
+      },
+      resize: true
+    },
+    modes: {
+      push: { quantity: 4 },
+      attract: { distance: 200, duration: 0.4, factor: 5 }
+    }
+  },
+  particles: {
+    color: { value: "#fd3e62" },
+    line_linked: {
+      color: "#fd3e62",
+      distance: 150,
+      enable: true,
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+      bounce: false,
+      direction: "none",
+      enable: true,
+      out_mode: "out",
+      random: false,
+      speed: 2,
+      straight: false
+    },
+    number: { density: { enable: true, value_area: 800 }, value: 80 },
+    opacity: {
+      anim: { enable: false, opacity_min: 0.1, speed: 1, sync: false },
+      random: false,
+      value: 0.5
+    },
+    shape: {
+      character: {
+        fill: false,
+        font: "Verdana",
+        style: "",
+        value: "*",
+        weight: "400"
+      },
+      image: {
+        height: 100,
+        replace_color: true,
+        src: "images/github.svg",
+        width: 100
+      },
+      polygon: { nb_sides: 5 },
+      stroke: { color: "#000000", width: 0 },
+      type: "circle"
+    },
+    size: {
+      anim: { enable: false, size_min: 0.1, speed: 40, sync: false },
+      random: true,
+      value: 5
+    }
+  },
+  polygon: {
+    draw: { enable: false, lineColor: "#fd3e62", lineWidth: 0.5 },
+    move: { radius: 10 },
+    scale: 1,
+    type: "none",
+    url: ""
+  },
+  retina_detect: true
+});
